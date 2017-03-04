e56270a83bdef2d86cf45779f5b34ed5a0c947ec
ataomic <ataomic@gmail.com>
2017-03-04 05:10:31 -0800

    It's better to convert existing stable OSS, not write from scratch

---
+++ src/include/hm_mgr.h
@@ -0,0 +1,7 @@
+#ifndef HM_MGR_H
+#define HM_MGR_H
+
+#include "hm_pool.h"
+
+#endif
+
--- src/include/hm_types.h
+++ src/include/hm_types.h
@@ -3,13 +3,5 @@
 
 #include "hm_def.h"
 
-typedef signed char s8;
-typedef short s16;
-typedef int s32;
-
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-
 #endif
 
